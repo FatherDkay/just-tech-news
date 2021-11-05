2d9ac5155d89c13e27383e1adfbc93fb040fed69
FatherDkay <paul.d.bailey@hotmail.com>
2021-11-04 20:57:54 -0700

    finished post

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,35 @@
 const router = require('express').Router();
 
-const userRoutes = require('./user-routes.js');
+const userRoutes = require('./user-routes');
+const postRoutes = require('./post-routes');
 
 router.use('/users', userRoutes);
+router.use('/posts', postRoutes);
+
+router.get('/:id', (req, res) => {
+    Post.findOne({
+      where: {
+        id: req.params.id
+      },
+      attributes: ['id', 'post_url', 'title', 'created_at'],
+      include: [
+        {
+          model: User,
+          attributes: ['username']
+        }
+      ]
+    })
+      .then(dbPostData => {
+        if (!dbPostData) {
+          res.status(404).json({ message: 'No post found with this id' });
+          return;
+        }
+        res.json(dbPostData);
+      })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
+  });
 
 module.exports = router;
